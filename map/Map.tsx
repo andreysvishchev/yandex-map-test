6f1c70992d8d1d789efb6a49b0eaa03712415a55
andreysvishchev <andrey.svishchev@yandex.ru>
2024-01-30 14:10:49 +0300

    AS: fix

--- map/Map.tsx
+++ map/Map.tsx
@@ -5,26 +5,29 @@ import {useEffect, useState} from "react";
 const Map = () => {
     const [init, setInit] = useState(false)
     useEffect(() => {
-        if (!init) {
-            let myMap
-            // @ts-ignore
-            myMap = new ymaps.Map('map', {
-                center: [55.76, 37.64], // Москва
-                zoom: 10
-            }, {
-                searchControlProvider: 'yandex#search'
-            });
+        let myMap
+        // @ts-ignore
+        myMap = new ymaps.Map('map', {
+            center: [55.76, 37.64], // Москва
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
 
-            setInit(true)
-        }
 
-    }, [init]);
+    }, []);
 
 
     return (
-        <div style={{width: 300, height: 300}} id={'map'}>
+        <>
 
-        </div>
+                <div style={{width: 300, height: 300}} id={'map'}>
+
+                </div>
+
+        </>
+
+
     );
 };
 
